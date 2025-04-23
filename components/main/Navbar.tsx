a91f030614f17c00e537aa9a0ac42ae47e162bf0
Rupesh Khadka <rupeshkhadka000@gmail.com>
2025-04-23 17:51:36 +0545

    fixed all

--- components/main/Navbar.tsx
+++ components/main/Navbar.tsx
@@ -5,18 +5,18 @@ import React from "react";
 
 const Navbar = () => {
   return (
-    <div className="w-full h-[65px] fixed top-0 shadow-lg shadow-[#2A0E61]/50 bg-[#03001417] backdrop-blur-md z-50 px-10">
-      <div className="w-full h-full flex flex-row items-center justify-between m-auto px-[10px]">
+    <div className="w-full h-[65px] fixed top-0 shadow-lg shadow-[#2A0E61]/50 bg-[#03001417] backdrop-blur-md z-50 px-1 md:px-10">
+      <div className="w-full h-full flex flex-row items-center justify-evenly md:mx-auto gap-4">
         <a
           href="#about-me"
-          className="h-auto w-auto flex flex-row items-center"
+          className="h-auto w-auto flex flex-row  items-center"
         >
           <Image
             src="/avatar.png"
             alt="logo"
-            width={70}
+            width={200}
             height={70}
-            className="cursor-pointer hover:animate-slowspin"
+            className="w-[200px] h-[50px] md:w-[70px] md:h-[60px]  cursor-pointer hover:animate-slowspin"
           />
 
           <span className="font-bold ml-[10px] hidden md:block text-gray-300">
@@ -24,8 +24,8 @@ const Navbar = () => {
           </span>
         </a>
 
-        <div className="w-[500px] h-full flex flex-row items-center justify-between md:mr-20">
-          <div className="flex items-center justify-between w-full h-auto border border-[#7042f861] bg-[#0300145e] mr-[15px] px-[20px] py-[10px] rounded-full text-gray-200">
+        <div className="w-[900px] h-full flex flex-row items-center justify-between md:mr-20">
+          <div className="flex items-center justify-between w-full h-auto border border-[#7042f861] px-2 md:px-6 bg-[#0300145e] mr-[15px] gap-3 py-[10px] rounded-full text-gray-200">
             <a href="#about-me" className="cursor-pointer">
               About me
             </a>
@@ -38,7 +38,7 @@ const Navbar = () => {
           </div>
         </div>
 
-        <div className="flex flex-row gap-5">
+        <div className="flex flex-row gap-2 md:gap-6 mx-1">
           {Socials.map((social, index) =>
             social.link ? (
               <Link href={social.link} key={index}>
@@ -47,7 +47,7 @@ const Navbar = () => {
                   alt={social.name}
                   width={30}
                   height={30}
-                  className="cursor-pointer"
+                  className="w-[100px] h-[30px] md:w-[30px] md:h-[30px] cursor-pointer"
                 />
               </Link>
             ) : (
@@ -57,7 +57,7 @@ const Navbar = () => {
                 key={social.name}
                 width={30}
                 height={30}
-                className="cursor-pointer"
+                className="  cursor-pointer"
               />
             )
           )}
--- components/main/Projects.tsx
+++ components/main/Projects.tsx
@@ -13,28 +13,29 @@ const Projects = () => {
       </h1>
       <div className="h-full w-full grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-10 px-10">
         <ProjectCard
+          src="/JobNest-SS.png"
+          title="JobNest"
+          link="https://job-nest-nu.vercel.app"
+          description="Built with Next.js 15, TypeScript, TailwindCSS, and Shadcn UI, JobNest offers secure authentication, custom onboarding for job seekers and organizations, and powerful job post management with CRUD, rich text editing (Tiptap), salary sliders, image uploads, and expiration logic. It integrates Stripe for payments, uses Inngest for automated job summaries, and applies Arcjet security for protection against XSS and SQL injection. Features include favorites, filtering, pagination, rate limiting, and a responsive, performance-optimized UI—deployed on Vercel."
+        />
+        <ProjectCard
           src="/ShedCal.png"
           title="ShedCal"
-          link="https://weatherapp-rupesh-khadkas-projects.vercel.app"
+          link="https://sched-cal.vercel.app"
           description="SchedCal is a powerful and fully customizable scheduling app that integrates seamlessly with the Nylas Calendar API, offering users the ability to create, cancel, and search for meetings effortlessly. With its rich set of features, including real-time availability updates, OAuth authentication, custom booking forms, and a beautifully designed responsive interface.s"
         />
         <ProjectCard
           src="/CardImage.png"
           link="https://weatherapp-rupesh-khadkas-projects.vercel.app"
-          title="Expense Tracker"
-          description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus luctus urna sed urna ultricies ac tempor dui sagittis. In condimentum facilisis porta. Sed nec diam eu diam mattis viverra. Nulla fringilla, purus eu venenatis sagittis, nisi leo ullamcorper eros, vitae scelerisque nisl lectus non erat."
-        />
-        <ProjectCard
-          src="/SpaceWebsite.png"
-          link="https://weatherapp-rupesh-khadkas-projects.vercel.app"
-          title="Modern NextJs portfolio"
-          description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus luctus urna sed urna ultricies ac tempor dui sagittis. In condimentum facilisis porta. Sed nec diam eu diam mattis viverra. Nulla fringilla, purus eu venenatis sagittis, nisi leo ullamcorper eros, vitae scelerisque nisl lectus non erat."
+          title="School Website"
+          description="Discover my portfolio on the official NVM school website — a dynamic platform showcasing my work and accomplishments. Features include an interactive gallery with full CRUD functionality, downloadable notices with real-time pop-up alerts, and a modern, SEO-optimized design. Built for performance, accessibility, and user engagement, this project reflects
+"
         />
         <ProjectCard
           src="/WeatherApp.png"
           link="https://weatherapp-rupesh-khadkas-projects.vercel.app"
           title="Weather app"
-          description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus luctus urna sed urna ultricies ac tempor dui sagittis. In condimentum facilisis porta. Sed nec diam eu diam mattis viverra. Nulla fringilla, purus eu venenatis sagittis, nisi leo ullamcorper eros, vitae scelerisque nisl lectus non erat."
+          description="A sleek weather application built with TypeScript and Next.js, powered by the OpenWeather API. This app provides real-time weather data with dynamic background transitions that visually reflect current conditions - from sunny skies to stormy nights. Designed with a responsive UI and performance-first architecture, it delivers an engaging and informative user experience."
         />
       </div>
     </section>
